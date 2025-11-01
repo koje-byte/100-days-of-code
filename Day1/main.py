50c1c42a559b42777151a765ea3d8250adadef82
koje-byte <ahmads.10@outlook.com>
2025-11-01 18:16:32 +0300

    Solved Day 1 :D

--- Day1/main.py
+++ Day1/main.py
@@ -1,4 +1,4 @@
 print("Welcome to the band name generator!")
 city=input("What city did you grow up in?\n")
 pet=input("What is the name of your first pet?\n")
-print("your band name is",city,pet)
+print(f"your band name is {city} {pet}")
